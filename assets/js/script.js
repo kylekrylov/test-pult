37747525794cd1b192cfc0b866018a935be51cbb
kylekrylov <kylekrylov@gmail.com>
2021-02-05 00:16:26 +0300

    //

---
+++ assets/js/script.js
@@ -0,0 +1,14 @@
+"use strict"
+// 
+;
+
+$(document).ready(function () {
+   $('.portfolio__cards').slick({
+      dots: true,
+      // infinite: true,
+      speed: 300,
+      // slidesToShow: 1,
+      centerMode: true,
+      variableWidth: true
+   });
+});
