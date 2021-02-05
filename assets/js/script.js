c24ce2f1f5b772854aca3fa75f9934231f09930b
kylekrylov <kylekrylov@gmail.com>
2021-02-05 16:35:50 +0300

    //

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,10 +5,29 @@
 $(document).ready(function () {
    $('.portfolio__cards').slick({
       dots: true,
-      // infinite: true,
+      infinite: true,
       speed: 300,
-      // slidesToShow: 1,
-      centerMode: true,
-      variableWidth: true
+      // slidesToShow: 4,
+      slidesToScroll: 1,
+      // centerMode: true,
+      variableWidth: true,
+      responsive: [
+         {
+           breakpoint: 600,
+           settings: {
+             slidesToShow: 2,
+             slidesToScroll: 1,
+             infinite: true
+           }
+         },
+         {
+            breakpoint: 320,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1,
+              infinite: true
+            }
+          }
+      ]
    });
 });
